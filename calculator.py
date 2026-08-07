062df312cab6b38cff01b6e8ab62acd5e78a91a7
asgofficial <gautamayushsinghofficial@gmail.com>
2026-08-07 10:25:03 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+
+print("\nThe Sum of the Numbers is : ", a+b)
+print("\nThe Difference of the Numbers is : ",a-b)
+print("\nThe Product of the Numbers is : ", a*b)
+print("\nThe Division of the Numbers gives : ", a/b)
